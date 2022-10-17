03857f2d04082897595f1c8c673b38b85de4907f
peynaj <peynajber@gmail.com>
2022-10-17 14:31:20 +0330

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='sql-judge-utils',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(
         include=(
             "sql_judge_utils",
